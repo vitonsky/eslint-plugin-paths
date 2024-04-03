de22ef7f6298e6dbed7eb2f70e578922777c156e
Robert Vitonsky <rob@vitonsky.net>
2024-04-03 20:52:08 +0200

    test: add test cases

--- src/import-paths.test.ts
+++ src/import-paths.test.ts
@@ -16,7 +16,18 @@ tester.run('import-paths', rule, {
 		{
 			options: [{}],
 			filename: path.resolve('./src/foo.ts'),
-			code: `import bar from 'bar';`,
+			code: `
+			import globalFoo from '@foo';
+			import globalBar from '@bar';
+			import bar1 from '@bar/1';
+			import bar2 from '@bar/1/2';
+			import bar3 from '@bar/1/2/3';
+
+			import globalBaz from './baz';
+			import baz1 from './baz/main';
+			import baz2 from '../baz/main';
+			import baz3 from '../../baz/main';
+			`.replace(/\t/g, ''),
 		},
 	],
 	invalid: [
@@ -26,6 +37,35 @@ tester.run('import-paths', rule, {
 			code: `import bar from '../bar/index';`,
 			errors: ['Update import to @bar/index'],
 			output: `import bar from '@bar/index';`,
+		},
+		{
+			options: [{}],
+			filename: path.resolve('./src/foo/index.ts'),
+			code: `
+			// import globalFoo from '../foo/index';
+			import globalBar from '../bar/index';
+			import bar1 from '../bar/1';
+			// import bar2 from 'bar/1/2';
+			// import bar3 from 'bar/1/2/3';
+
+			// import globalBaz from './baz';
+			// import baz1 from './baz/main';
+			// import baz2 from '../baz/main';
+			// import baz3 from '../../baz/main';
+			`.replace(/\t/g, ''),
+			errors: ['Update import to @bar/index', 'Update import to @bar/1'],
+			output: `
+			// import globalFoo from '../foo/index';
+			import globalBar from '@bar/index';
+			import bar1 from '@bar/1';
+			// import bar2 from 'bar/1/2';
+			// import bar3 from 'bar/1/2/3';
+
+			// import globalBaz from './baz';
+			// import baz1 from './baz/main';
+			// import baz2 from '../baz/main';
+			// import baz3 from '../../baz/main';
+			`.replace(/\t/g, ''),
 		},
 	],
 });
