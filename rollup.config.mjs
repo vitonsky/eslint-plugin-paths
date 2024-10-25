be0c4d71ef2cdc77b68b0467100b0dfc29c93f2a
Robert Vitonsky <rob@vitonsky.net>
2024-10-25 18:56:44 +0200

    build: generate declaration files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,46 +1,58 @@
 import copy from 'rollup-plugin-copy';
+import dts from 'rollup-plugin-dts';
 import typescript from '@rollup/plugin-typescript';
 
-export default {
-	input: 'src/index.ts',
-	output: {
-		dir: 'dist',
-		format: 'cjs',
-	},
-	plugins: [
-		typescript(),
-		copy({
-			targets: [
-				{
-					src: 'LICENSE',
-					dest: 'dist',
-				},
-				{
-					src: 'README.md',
-					dest: 'dist',
-				},
-				{
-					src: 'package.json',
-					dest: 'dist',
-					transform(content) {
-						const ignoredProps = ['devDependencies', 'scripts'];
-
-						const json = JSON.parse(content.toString());
-						return JSON.stringify(
-							{
-								...Object.fromEntries(
-									Object.entries(json).filter(
-										([key]) => !ignoredProps.includes(key),
-									),
-								),
-								main: 'index.js',
-							},
-							null,
-							4,
-						);
+export default [
+	{
+		input: 'src/index.ts',
+		output: {
+			dir: 'dist',
+			format: 'cjs',
+		},
+		plugins: [
+			typescript(),
+			copy({
+				targets: [
+					{
+						src: 'LICENSE',
+						dest: 'dist',
 					},
-				},
-			],
-		}),
-	],
-};
+					{
+						src: 'README.md',
+						dest: 'dist',
+					},
+					{
+						src: 'package.json',
+						dest: 'dist',
+						transform(content) {
+							const ignoredProps = ['devDependencies', 'scripts'];
+
+							const json = JSON.parse(content.toString());
+							return JSON.stringify(
+								{
+									...Object.fromEntries(
+										Object.entries(json).filter(
+											([key]) => !ignoredProps.includes(key),
+										),
+									),
+									main: 'index.js',
+								},
+								null,
+								4,
+							);
+						},
+					},
+				],
+			}),
+		],
+	},
+	{
+		// Generate declaration file
+		input: 'src/index.ts',
+		output: {
+			file: 'dist/index.d.ts',
+			format: 'cjs',
+		},
+		plugins: [dts()],
+	},
+];
